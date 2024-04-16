086f8a89d249c2a578242f9d85528c1a9e221fdd
Raúl <rules_cc99@hotmail.com>
2024-04-16 10:04:18 -0600

    se actualiza el modelo Property

--- api/src/models/Property.js
+++ api/src/models/Property.js
@@ -35,7 +35,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     bedrooms: {
