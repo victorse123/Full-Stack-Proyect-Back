e0c3c4465a2cb6521c48f34f6f58f33bea0a3776
Victor Sepulveda <victorseva123@gmail.com>
2024-04-19 06:22:06 -0500

    Nodemailer listo

---
+++ api/src/controllers/postBooking.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp-mail.outlook.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
+});
+
+exports.realizarReserva = async (req, res) => {
+  
+  const { destinatario, propiedad } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: destinatario,
+    subject: 'Confirmación de reserva',
+    text: `Has reservado la propiedad ${propiedad}. Gracias por tu reserva.`
+  };
+
+
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//       console.log(error);
+//       res.status(500).send('Error al enviar el correo electrónico.');
+//     } else {
+//       console.log('Correo enviado: ' + info.response);
+//       res.status(200).send('Correo enviado correctamente.');
+//     }
+//   });
+// };
+
+
+try {
+    // Enviar el correo electrónico
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Correo enviado: ' + info.response);
+    res.status(200).send('Correo enviado correctamente.');
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico:', error);
+    res.status(500).send('Error al enviar el correo electrónico.');
+  }
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,9 @@ const { Router } = require('express');
 const RouterProperty = require("./RouterProperty");
 const RouterType = require("./RouterType");
 const RouterCategory = require("./RouterCategory");
-
 const RouterPagination = require("./RouterPagination");
-
-const RouterFindId= require ("./RouterFindId")
+const RouterFindId = require("./RouterFindId");
+const RouterBooking = require("./RouterBooking")
 
 // const RouterUser = require("./RouterUser");
 
@@ -19,9 +18,10 @@ router.use("/category", RouterCategory);
 router.use("/pagination", RouterPagination);
 
 router.use("/category", RouterCategory)
-router.use("/propertyId",RouterFindId)
+router.use("/propertyId" ,RouterFindId)
+
+router.use('/booking', RouterBooking);
 
 // router.use("/user", RouterUser);
-
 
 module.exports = router;
