b1b08c262d930adf83b45c6bbe54a6b2689ad047
Victor Sepulveda <victorseva123@gmail.com>
2024-04-25 18:48:49 -0500

    actual .env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
